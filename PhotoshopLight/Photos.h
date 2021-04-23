dbd212864cfb89f5b4d0966eb5d62acdc5840607
Nazarii Proshyn <nproshyn@corelogic.com>
2021-04-23 17:36:54 +0300

    Add ViewController

--- PhotoshopLight/Photos.h
+++ PhotoshopLight/Photos.h
@@ -8,14 +8,14 @@ class Photos
 public:
 	Photos();
 	~Photos();
-void setpath(std::string pathImage);
-	 sf::Sprite sprite;
+
+    void setpath(std::string pathImage);
+
+    sf::Sprite sprite;
 private:
     std::string path = "1.png";
 
-     sf::Image image;
-     sf::Texture texture;
-
-    //size of window
+    sf::Image image;
+    sf::Texture texture;
 };
 
--- PhotoshopLight/main.cpp
+++ PhotoshopLight/main.cpp
@@ -1,60 +1,51 @@
 #include "Photos.h"
 #include "Filtrs.h"
-#include <sstream>
+#include "ViewController.h"
+
 
 int main()
 {
     const unsigned int wWidth = 800;
     const unsigned int wHeight = 600;
 
-    sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "SFML Island",
+    sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "Photoshop Light",
                             sf::Style::Titlebar | sf::Style::Close);
     window.setVerticalSyncEnabled(true);
 
-Photos photo;
-    sf::Font font;
-    if (!font.loadFromFile("D:\\Projects\\PhotoshopL\\PhotoshopLight\\PhotoshopLight\\sansation.ttf"))
+    ViewController view;
+
+    if (!view.checkFont())
         {
             window.close();
             return 0;
         }
 
-        sf::Text infoText;
 
-        infoText.setFont(font);
-        infoText.setCharacterSize(32);
-        infoText.setFillColor(sf::Color::White);
-        infoText.setOutlineColor(sf::Color::Red);
-        infoText.setOutlineThickness(2.2f);
-        infoText.setPosition(5.2f, 5.1f);
-
-        std::ostringstream containerForInfoText;
-
-
-    Filtr filtr("1.png");
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            if (event.type == sf::Event::Closed ||sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+                {
+                    window.close();
+                }
+
+
+            view.imageEvents(event);
+
         }
 
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {filtr.dark();}
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {filtr.light();}
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {filtr.saturationUp();}
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {filtr.saturationDown();}
+		//if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {}
+        //if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {filtr.light();}
 
-        containerForInfoText.str("");
-        containerForInfoText << "It's works!\n\n";
-
-        infoText.setString(containerForInfoText.str());
-
-
+        view.textView();
         window.clear();
-        window.draw(infoText);
-        window.draw(filtr.getSprite());
+        for(size_t i = 0; i < view.getsizeOfInfoText(); i++)
+            {
+                window.draw(view.getText(i));
+            }
+        window.draw(view.getSprite());
         window.display();
     }
 
